02cfc05ba587f86ddc7d898377a2e6a2c5cc6717
Connor Love <connor@dotconnor.com>
2019-05-16 21:28:33 -0400

    increase executor count

--- src/worker/index.js
+++ src/worker/index.js
@@ -1,6 +1,7 @@
 require(`dotenv`).config()
 const path = require(`path`)
 const Bull = require(`bull`)
+const os = require(`os`)
 
 const builds = new Bull(`builds`, {
   redis: {
@@ -29,4 +30,9 @@ const jobFinished = new Bull(`job_finished`, {
 freshStart.process(1, path.join(__dirname, `fresh_start.js`))
 builds.process(1, path.join(__dirname, `builds.js`))
 jobs.process(1, path.join(__dirname, `jobs.js`))
-jobFinished.process(1, path.join(__dirname, `job_finished.js`))
+jobFinished.process(
+  process.env.NODE_ENV === `development`
+    ? 1
+    : require(`physical-cpu-count`) / 2,
+  path.join(__dirname, `job_finished.js`)
+)
