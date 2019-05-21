0742afbec726ea47a30266197754e88db2dcde73
Connor Love <connor@dotconnor.com>
2019-05-21 09:30:40 -0400

    upload loads for jobs

--- src/worker/jobs.js
+++ src/worker/jobs.js
@@ -1,5 +1,7 @@
 const execa = require(`execa`)
 const Bull = require(`bull`)
+const debug = require(`debug`)(`intramural:worker:jobs`)
+const { S3 } = require(`aws-sdk`)
 const db = require(`../server/db`)
 const generateScript = require(`./utils/generateScript`)
 
@@ -8,6 +10,27 @@ const jobFinished = new Bull(`job_finished`, {
     host: process.env.NODE_ENV === `development` ? `localhost` : `redis`,
   },
 })
+
+const s3 = new S3()
+
+function uploadLogs(build, job, logs) {
+  return new Promise((resolve, reject) => {
+    s3.putObject(
+      {
+        Key: `${build}/${job}`,
+        Bucket: `intramural-logs`,
+        ContentType: `text/plain`,
+        Body: logs,
+      },
+      (err) => {
+        if (err) {
+          return reject(err)
+        }
+        return resolve()
+      }
+    )
+  })
+}
 
 module.exports = async (job) => {
   try {
@@ -22,7 +45,7 @@ module.exports = async (job) => {
       [`pending`, Date.now(), job.data.id]
     )
     const script = generateScript(repo, job.data, job.data.meta)
-    console.log(`Starting #${job.data.build}.${job.data.job}`)
+    debug(`Starting #${job.data.build}.${job.data.job}`)
     const d = execa(
       `docker run -i --rm -m 2G --cpus 1 intramural/intramural:latest /bin/bash`,
       {
@@ -43,18 +66,21 @@ module.exports = async (job) => {
         [`error`, Date.now(), job.data.id]
       )
     }
-    console.log(
+    debug(
       `Finished #${job.data.build}.${job.data.job}: ${r.exitCode} ${
         r.exitCodeName
       }`
     )
+    debug(`Uploading Logs: ${job.data.build_id}/${job.data.id}`)
+    await uploadLogs(job.data.build_id, job.data.id, r.all)
+    debug(`Uploaded Logs`)
     jobFinished.add(
       {
         build: job.data.build_id,
         job: job.data.id,
       },
       {
-        delay: 250,
+        delay: 250 + Math.random() * 50,
       }
     )
   } catch (e) {
