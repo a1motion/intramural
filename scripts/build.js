25dd08413626c7ced2f5a25be5655d874c53c9f8
Connor Love <connor@dotconnor.com>
2019-06-04 23:38:25 -0400

    ref: update code style

--- scripts/build.js
+++ scripts/build.js
@@ -1,72 +1,72 @@
 const aws = require('aws-sdk') //eslint-disable-line
-const fs = require(`fs`)
-const path = require(`path`)
-const mime = require(`mime`)
-const execa = require(`execa`)
-require(`dotenv`).config()
+const fs = require(`fs`);
+const path = require(`path`);
+const mime = require(`mime`);
+const execa = require(`execa`);
+require(`dotenv`).config();
 
-const start = Date.now()
+const start = Date.now();
 
-const bin = path.join(__dirname, `..`, `node_modules`, `.bin`)
+const bin = path.join(__dirname, `..`, `node_modules`, `.bin`);
 
-const s3 = new aws.S3()
+const s3 = new aws.S3();
 function promisify(func, dir) {
   return new Promise((resolve, reject) => {
     func(dir, (err, data) => {
       if (err) {
-        return reject(err)
+        return reject(err);
       }
-      return resolve(data)
-    })
-  })
+      return resolve(data);
+    });
+  });
 }
-const lstat = (dir) => promisify(fs.lstat, dir)
-const readdir = (dir) => promisify(fs.readdir, dir)
-const readFile = (file) => promisify(fs.readFile, file)
-const upload = (opts) => promisify(s3.putObject.bind(s3), opts)
+const lstat = (dir) => promisify(fs.lstat, dir);
+const readdir = (dir) => promisify(fs.readdir, dir);
+const readFile = (file) => promisify(fs.readFile, file);
+const upload = (opts) => promisify(s3.putObject.bind(s3), opts);
 
 function flattenDeep(arr) {
   return arr.reduce(
     (acc, e) => (Array.isArray(e) ? acc.concat(flattenDeep(e)) : acc.concat(e)),
     []
-  )
+  );
 }
 async function getFilesInDir(dir) {
-  let files = await readdir(dir)
-  files = files.map((file) => path.join(dir, file))
-  const stats = await Promise.all(files.map((file) => lstat(file)))
+  let files = await readdir(dir);
+  files = files.map((file) => path.join(dir, file));
+  const stats = await Promise.all(files.map((file) => lstat(file)));
   const data = await Promise.all(
     files.map((file, i) => {
       if (stats[i].isDirectory()) {
-        return getFilesInDir(file)
+        return getFilesInDir(file);
       }
-      return file
+      return file;
     })
-  )
-  return flattenDeep(data)
+  );
+  return flattenDeep(data);
 }
 function contentType(src, ext) {
-  return (mime.getType(ext || src) || ``).replace(`-`, ``)
+  return (mime.getType(ext || src) || ``).replace(`-`, ``);
 }
 
 const deploy = async () => {
-  const files = await getFilesInDir(`./build/client`)
+  const files = await getFilesInDir(`./build/client`);
   await files.map(async (file) => {
-    let Key = path.relative(`./build/client`, file)
-    let Body = await readFile(file)
+    let Key = path.relative(`./build/client`, file);
+    let Body = await readFile(file);
     if (Key.endsWith(`.map`)) {
-      return false
+      return false;
     }
     if (Key.endsWith(`.css`)) {
-      Body = Body.toString().replace(/\/\*[^*]*\*+([^/*][^*]*\*+)*\//, ``)
+      Body = Body.toString().replace(/\/\*[^*]*\*+([^/*][^*]*\*+)*\//, ``);
     }
     const CacheControl =
       Key === `index.html`
         ? `no-cache, no-store, must-revalidate`
-        : `public, max-age=31536000`
-    const Bucket = `public.a1motion.com`
-    Key = Key.replace(/\\/g, `/`)
-    Key = `mural/${Key}`
+        : `public, max-age=31536000`;
+    const Bucket = `public.a1motion.com`;
+    Key = Key.replace(/\\/g, `/`);
+    Key = `mural/${Key}`;
     return upload({
       Body,
       CacheControl,
@@ -74,49 +74,49 @@ const deploy = async () => {
       Bucket,
       ContentLength: Body.length,
       ContentType: contentType(file),
-    })
-  })
-}
+    });
+  });
+};
 
 const release = async () => {
-  const sentryCli = path.join(bin, `sentry-cli`)
+  const sentryCli = path.join(bin, `sentry-cli`);
 
-  const VERSION = require(`../package.json`).version
+  const VERSION = require(`../package.json`).version;
 
   await execa(`${sentryCli} releases new -p a1motion ${VERSION}`, {
     shell: true,
     env: process.env,
-  })
+  });
   await execa(`${sentryCli} releases set-commits --auto ${VERSION}`, {
     shell: true,
     env: process.env,
-  })
-  const dur = Math.floor((Date.now() - start) / 1000)
+  });
+  const dur = Math.floor((Date.now() - start) / 1000);
   await execa(
     `${sentryCli} releases deploys ${VERSION} new -e production -t ${dur}`,
     {
       shell: true,
       env: process.env,
     }
-  )
-}
+  );
+};
 
 const build = async () => {
-  const reactAppRewired = path.join(bin, `react-app-rewired`)
+  const reactAppRewired = path.join(bin, `react-app-rewired`);
   await execa(reactAppRewired, [`build`], {
     shell: true,
     env: { ...process.env, PUBLIC_URL: `https://cdn.a1motion.com/` },
-  })
-}
+  });
+};
 
 const main = async () => {
   try {
     //await build()
-    await deploy()
+    await deploy();
     //await release()
   } catch (e) {
-    console.log()
-    console.log(e)
+    console.log();
+    console.log(e);
   }
-}
-main()
+};
+main();
